841eceb277bfc33f177651036df907bf5ed90963
leeeunsik <dldmstlr20419@naver.com>
2025-05-08 09:23:07 +0900

    00 Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   package.json
    	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -12,19 +11,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 성능 측정용
 reportWebVitals();
-=======
-import ReactDOM from 'react-dom';
-import App from './App'; // Ensure you have an App component in src/App.js
-
-// Render the App component into the root div
-ReactDOM.render(
-  <React.StrictMode>
-	<App />
-  </React.StrictMode>,
-  document.getElementById('root') // Ensure your public/index.html has a div with id="root"
-);
->>>>>>> 29fe17261a49fa81bef4e3778576a70951560fed
